09bc80585ad5bd8ca795e89145344d5f404bdf86
Huunhat123 <huunhat20052006@gmail.com>
2025-09-05 10:17:10 +0700

    TableTodoList.tsx

--- HN_KS24CNTT1_TranHuuNhat_001/src/components/TableTodoList.tsx
+++ HN_KS24CNTT1_TranHuuNhat_001/src/components/TableTodoList.tsx
@@ -1,19 +1,18 @@
-import React, { useState } from 'react'
-import { DeleteOutlined } from '@ant-design/icons';
-import { FormOutlined } from '@ant-design/icons';
-import { UsertodoList, Action } from './UserInterFace';
-import EditListModal from './EditListModal';
-
-import "./tabletodolist.css"
+import React, { useState } from "react";
+import { DeleteOutlined, FormOutlined } from "@ant-design/icons";
+import { UsertodoList, Action } from "./UserInterFace";
+import EditListModal from "./EditListModal";
+import "./tabletodolist.css";
 
 interface Props {
-  lists: UsertodoList[]
-  dispatch: React.Dispatch<Action>
+  lists: UsertodoList[];
+  dispatch: React.Dispatch<Action>;
 }
 
-const TableTodoList: React.FC<Props> = ({lists, dispatch}) => {
+const TableTodoList: React.FC<Props> = ({ lists, dispatch }) => {
   const [editModalOpen, setEditModalOpen] = useState(false);
   const [selectedList, setSelectedList] = useState<UsertodoList | null>(null);
+
   const handleEditClick = (list: UsertodoList) => {
     setSelectedList({ ...list });
     setEditModalOpen(true);
@@ -21,22 +20,42 @@ const TableTodoList: React.FC<Props> = ({lists, dispatch}) => {
 
   const handleEditConfirm = () => {
     if (selectedList) {
-      dispatch({ type: 'UPDATE', payload: selectedList });
+      dispatch({ type: "UPDATE", payload: selectedList });
     }
     setEditModalOpen(false);
   };
 
+  const handleDeleteClick = (id: number) => {
+    dispatch({ type: "DELETE", payload: id });
+  };
 
   return (
-    <div className='tabletodolist'>
-      <div className='todolist'>
-        <p>Code a todo list</p>
-        <div className='classbtn'>
-          <input type="checkbox" />
-          <button className='btnfix' onClick={handleEditClick}><FormOutlined /></button>
-          <button className='btndelete'><DeleteOutlined /></button>
+    <div className="tabletodolist">
+      {lists.map((list) => (
+        <div key={list.id} className="todolist">
+          <p
+            style={{ textDecoration: list.completed ? "line-through" : "none" }}
+          >
+            {list.name}
+          </p>
+          <div className="classbtn">
+            <input
+              type="checkbox"
+              checked={list.completed}
+              onChange={() => dispatch({ type: "TOGGLE", payload: list.id })}
+            />
+            <button className="btnfix" onClick={() => handleEditClick(list)}>
+              <FormOutlined />
+            </button>
+            <button
+              className="btndelete"
+              onClick={() => handleDeleteClick(list.id)}
+            >
+              <DeleteOutlined />
+            </button>
+          </div>
         </div>
-      </div>
+      ))}
 
       {selectedList && (
         <EditListModal
@@ -48,9 +67,7 @@ const TableTodoList: React.FC<Props> = ({lists, dispatch}) => {
         />
       )}
     </div>
+  );
+};
 
-    
-  )
-}
-
-export default TableTodoList
+export default TableTodoList;
